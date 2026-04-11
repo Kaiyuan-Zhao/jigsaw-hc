b1b779957714381c33d928c2dc02261064a1762c
Ken <Kenzhao08@hotmail.com>
2026-04-11 04:54:51 -0700

    feat: api fix

--- api/[...route].ts
+++ api/[...route].ts
@@ -2,4 +2,7 @@ import { createApp } from '../backend/src/app.js'
 
 const app = createApp()
 
-export default app
+export default function handler(req: any, res: any) {
+	req.url = (req.url || '').replace(/^\/api/, '') || '/'
+	return app(req, res)
+}
